c073bad6335c2eb2ed34a9015af67d8d0030c7c3
Guy Podjarny <guypod@gmail.com>
2015-11-19 00:23:43 +0000

    chore: initial creation of package

---
+++ README.md
@@ -0,0 +1,7 @@
+Snyk Demo App
+===
+
+This is a demo application, not intended for real use.
+It's used as a part of a tutorial for running [snyk](https://snyk.io/), and was initially based on [falcor-hapi-demo](https://github.com/Netflix/falcor-hapi-demo).
+
+For more detail, read the [Snyk documentation](https://snyk.io/docs/).
---
+++ index.js
@@ -0,0 +1,48 @@
+var Hapi = require('hapi');
+var FalcorHandler = require('falcor-hapi');
+var falcorRouterDemoFactory = require('falcor-router-demo');
+
+var app = new Hapi.Server();
+
+app.connection({
+    host: "localhost",
+    port: 9090
+});
+
+// serve static files (index.html)
+var plugins = [
+  {
+    register: require('inert')
+  },
+  {
+    register: require('bassmaster')
+  }
+];
+
+app.register(plugins, function (err) {
+    if (err) {
+        throw err;
+    }
+    app.route({
+       method: "GET",
+       path: "/{param*}",
+       handler: {
+           directory: {
+               path: __dirname
+           }
+       }
+   });
+});
+
+app.route({
+    method: ["GET", "POST"],
+    path: "/model.json",
+    handler: FalcorHandler.dataSourceRoute(function(req, res) {
+        return falcorRouterDemoFactory("1");
+    })
+});
+
+app.start(function () {
+    console.log("Navigate to:", app.info.uri);
+});
+
